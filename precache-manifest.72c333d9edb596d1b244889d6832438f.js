56ed97ccf65cad161620b118f1bfaec1eaa9ecaa
Sam Wheeling <anchormansam@gmail.com>
2019-10-25 09:52:41 -0400

    Updates

---
+++ precache-manifest.72c333d9edb596d1b244889d6832438f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09e3fad7857b8c2a79fa4543c7cdb50e",
+    "url": "/reactrestaurant/index.html"
+  },
+  {
+    "revision": "f03aa04e7e6db8928660",
+    "url": "/reactrestaurant/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9065a74e10f1c7c0e942",
+    "url": "/reactrestaurant/static/css/main.711bed4d.chunk.css"
+  },
+  {
+    "revision": "f03aa04e7e6db8928660",
+    "url": "/reactrestaurant/static/js/2.4ea9e624.chunk.js"
+  },
+  {
+    "revision": "9065a74e10f1c7c0e942",
+    "url": "/reactrestaurant/static/js/main.2c995f31.chunk.js"
+  },
+  {
+    "revision": "26ce23a6b3cdcaf839b1",
+    "url": "/reactrestaurant/static/js/runtime-main.2815b5a5.js"
+  },
+  {
+    "revision": "ae6651b1a91a47b1ce18b443879b8ba3",
+    "url": "/reactrestaurant/static/media/chalkabout-webfont.ae6651b1.woff"
+  },
+  {
+    "revision": "e0117f9fcfde0f23f30abe899defa2e8",
+    "url": "/reactrestaurant/static/media/chalkabout-webfont.e0117f9f.woff2"
+  }
+]);
